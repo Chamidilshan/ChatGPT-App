68826a4e4a0d8c781a2d152c3f4bb2f9f20b9cc2
Your Name <chamidilshan55@gmail.com>
2023-02-13 18:42:21 +0530

    edited texts styles in keyGen page

--- lib/screens/keyGenerate_screen.dart
+++ lib/screens/keyGenerate_screen.dart
@@ -40,119 +40,129 @@ class _GenerateKeyState extends State<GenerateKey> {
         iconTheme: IconThemeData(color: Colors.black87),
       ),
       drawer: NavigationDrawer(),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20.0,
-          ),
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Container(
-                padding: EdgeInsets.all(15.0),
-                child: Text('To chat with me you should generate a API key'),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.green,
-                        width: 2.0
-                    ),
-                    borderRadius: BorderRadius.circular(20.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.green,
-                      blurRadius: 10.0,
-                      offset: Offset(0, 10),
-                    )
-                  ],
-                  color: Colors.white
+      body: Container(
+        padding: EdgeInsets.only(left: 40.0, right: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+              Text(
+                'Generate a', style: TextStyle(fontSize: 20.0, color: Colors.black87),
+              ),
+            Text(
+              'API Key', style: TextStyle(fontSize: 20.0, color: Colors.black87),
+            ),
+              // Padding(
+              //   padding: const EdgeInsets.all(5.0),
+              //   child: Container(
+              //     padding: EdgeInsets.all(15.0),
+              //     child: Text('To chat with me you should generate a API key'),
+              //     decoration: BoxDecoration(
+              //         border: Border.all(
+              //             color: Colors.green,
+              //             width: 2.0
+              //         ),
+              //         borderRadius: BorderRadius.circular(20.0),
+              //       boxShadow: [
+              //         BoxShadow(
+              //           color: Colors.green,
+              //           blurRadius: 10.0,
+              //           offset: Offset(0, 10),
+              //         )
+              //       ],
+              //       color: Colors.white
+              //     ),
+              //   ),
+              // ),
+            SizedBox(
+              height: 40.0,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.all(20.0),
+                fixedSize: Size(160, 40),
+                textStyle: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
                 ),
+                onPrimary: Colors.black,
+                elevation: 15.0,
+                shape: StadiumBorder(),
               ),
-            ),
-          SizedBox(
-            height: 20.0,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              padding: EdgeInsets.all(20.0),
-              fixedSize: Size(160, 40),
-              textStyle: TextStyle(
-                fontSize: 15.0,
-                fontWeight: FontWeight.bold,
-              ),
-              onPrimary: Colors.black,
-              elevation: 15.0,
-              shape: StadiumBorder(),
-            ),
-            onPressed: () async{
-          //final url = 'https://google.com';
+              onPressed: () async{
+            //final url = 'https://google.com';
 
-              if(await launch('https://platform.openai.com/account/api-keys',
-              )){
-                debugPrint('succesfully');
-              }
-            },
-            child: Text('Open in browser'),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-           Container(
-              child: Text(
-                  'After copied your Api key paste and save it here'
-                  ),
-           ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Container(
-            padding: EdgeInsets.all(15.0),
-            child: Text('API Key'),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.deepOrange,
-                width: 2.0
-              ),
-              borderRadius: BorderRadius.circular(20.0)
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Paste your api key',
-            ),
-            controller: pasteController,
-          ),
-          IconButton(
-              onPressed: () async {
-                final clipPaste =  await Clipboard.getData(Clipboard.kTextPlain);
-                final text = clipPaste == null ? '' : clipPaste.text!;
-                setState(() {
-                  pasteController.text = text;
-                });
-                },
-              icon: Icon(
-                Icons.content_paste,
-                color: Colors.green,
-              )
-          ),
-          TextButton(
-              onPressed: () async {
-                setState(() {
-                  Api_key2 = pasteController.text;
-                  print(Api_key2);
-                });
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                prefs.setString('ApiKey', Api_key2);
-                print(ApiKey);
+                if(await launch('https://platform.openai.com/account/api-keys',
+                )){
+                  debugPrint('succesfully');
+                }
               },
-            child: Text('Save'),
-          ),
-          TextButton(onPressed: () {
-            print(ApiKey);
-          }, child: Text('Key is'),)
-        ],
+              child: Text('Create new key'),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+             Container(
+                child: Text(
+                    'After copied your Api key paste and save it here'
+                    ),
+             ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              padding: EdgeInsets.all(15.0),
+              child: Text('API Key'),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.deepOrange,
+                  width: 2.0
+                ),
+                borderRadius: BorderRadius.circular(20.0)
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Paste your api key',
+              ),
+              controller: pasteController,
+            ),
+            IconButton(
+                onPressed: () async {
+                  final clipPaste =  await Clipboard.getData(Clipboard.kTextPlain);
+                  final text = clipPaste == null ? '' : clipPaste.text!;
+                  setState(() {
+                    pasteController.text = text;
+                  });
+                  },
+                icon: Icon(
+                  Icons.content_paste,
+                  color: Colors.green,
+                )
+            ),
+            TextButton(
+                onPressed: () async {
+                  setState(() {
+                    Api_key2 = pasteController.text;
+                    print(Api_key2);
+                  });
+                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  prefs.setString('ApiKey', Api_key2);
+                  print(ApiKey);
+                },
+              child: Text('Save'),
+            ),
+            TextButton(onPressed: () {
+              print(ApiKey);
+            }, child: Text('Key is'),)
+          ],
+        ),
       ),
     );
   }
